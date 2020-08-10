3032aec26a9d66921ef607851aa22f3f72595239
Pete Bunting <petebunting@mac.com>
2020-08-10 18:50:23 +0100

    Add scripts for processing landsat to ARD.

---
+++ ls_analysis/02_process_ard/gen_ard_jobs.py
@@ -0,0 +1,63 @@
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+import logging
+import os
+from arcsilib.arcsiscnprocessdb import RecordScn2Process
+
+logger = logging.getLogger(__name__)
+
+class FindSen2ScnsGenDwnlds(PBPTGenQProcessToolCmds):
+
+    def gen_command_info(self, **kwargs):
+        if not os.path.exists(kwargs['scn_db_file']):
+            raise Exception("Landsat scene database does not exist...")
+        scn_rcd_obj = RecordScn2Process(kwargs['scn_db_file'])
+        scns = scn_rcd_obj.get_scns_ard()
+        for scn in scns:
+            c_dict = dict()
+            c_dict['product_id'] = scn.product_id
+            c_dict['sensor'] = scn.sensor_id
+            c_dict['dwnld_path'] = scn.download_path
+            c_dict['ard_path'] = os.path.join(kwargs['ard_path'], scn.product_id)
+            c_dict['tmp_dir'] = os.path.join(kwargs['tmp_dir'], scn.product_id)
+            c_dict['scn_db_file'] = kwargs['scn_db_file']
+            c_dict['dem'] = kwargs['dem']
+            if not os.path.exists(c_dict['ard_path']):
+                os.mkdir(c_dict['ard_path'])
+            if not os.path.exists(c_dict['tmp_dir']):
+                os.mkdir(c_dict['tmp_dir'])
+            self.params.append(c_dict)
+
+    def run_gen_commands(self):
+        self.gen_command_info(scn_db_file='../01_find_ls_scns/ls_scn.db',
+                              ard_path='/Users/pete/Temp/arcsi_test_db_class/ls_ard',
+                              tmp_dir='/Users/pete/Temp/arcsi_test_db_class/tmp',
+                              dem='/Users/pete/Temp/arcsi_test_db_class/dem.kea')
+        self.pop_params_db()
+        self.create_shell_exe("run_ard_cmds.sh", "ard_cmds.sh", 4, db_info_file=None)
+        #self.create_slurm_sub_sh("ard_ls_scns", 16448, '/scratch/a.pfb/gmw_v2_gapfill/logs',
+        #                         run_script='run_exe_analysis.sh', job_dir="job_scripts",
+        #                         db_info_file=None, account_name='scw1376', n_cores_per_job=10, n_jobs=10,
+        #                         job_time_limit='2-23:59',
+        #                         module_load='module load parallel singularity\n\nexport http_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\nexport https_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\n')
+
+    def run_check_outputs(self):
+        process_tools_mod = 'perform_ard_process'
+        process_tools_cls = 'PerformScnARD'
+        time_sample_str = self.generate_readable_timestamp_str()
+        out_err_file = 'processing_errs_{}.txt'.format(time_sample_str)
+        out_non_comp_file = 'non_complete_errs_{}.txt'.format(time_sample_str)
+        self.check_job_outputs(process_tools_mod, process_tools_cls, out_err_file, out_non_comp_file)
+
+    def run_remove_outputs(self, all_jobs=False, error_jobs=False):
+        process_tools_mod = 'perform_ard_process'
+        process_tools_cls = 'PerformScnARD'
+        self.remove_job_outputs(process_tools_mod, process_tools_cls, all_jobs, error_jobs)
+
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("perform_ard_process.py")
+    #script_cmd = "singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python {}".format(py_script)
+    script_cmd = "python {}".format(py_script)
+
+    create_tools = FindSen2ScnsGenDwnlds(cmd=script_cmd, sqlite_db_file="ard_ls_scns.db")
+    create_tools.parse_cmds()
---
+++ ls_analysis/02_process_ard/perform_ard_process.py
@@ -0,0 +1,64 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import arcsilib
+import arcsilib.arcsirun
+import os
+import shutil
+from arcsilib.arcsiscnprocessdb import RecordScn2Process
+
+logger = logging.getLogger(__name__)
+
+class PerformScnARD(PBPTQProcessTool):
+
+    def __init__(self):
+        super().__init__(cmd_name='perform_ard_process.py', descript=None)
+
+    def do_processing(self, **kwargs):
+        scn_rcd_obj = RecordScn2Process(self.params['scn_db_file'])
+        downloaded = scn_rcd_obj.is_scn_downloaded(self.params['product_id'], self.params['sensor'])
+        ard_processed = scn_rcd_obj.is_scn_ard(self.params['product_id'], self.params['sensor'])
+        if downloaded and (not ard_processed):
+            input_hdr = self.find_first_file(self.params['dwnld_path'], "*MTL.txt")
+
+            arcsilib.arcsirun.runARCSI(input_hdr, None, None, self.params['sensor'], None, "KEA",
+                                       self.params['ard_path'], None, None, None, None, None, None,
+                                       ["CLOUDS", "DOSAOTSGL", "STDSREF", "SATURATE", "TOPOSHADOW",
+                                        "METADATA"],
+                                       True, None, None, arcsilib.DEFAULT_ARCSI_AEROIMG_PATH,
+                                       arcsilib.DEFAULT_ARCSI_ATMOSIMG_PATH,
+                                       "GreenVegetation", 0, None, None, False, None, None, None, None, False,
+                                       None, None, self.params['tmp_dir'], 0.05, 0.5, 0.1, 0.4, self.params['dem'],
+                                       None, None, True, 20, False, False, 1000, "cubic", "near", 3000, 3000, 1000, 21,
+                                       True, False, False, None, None, True, None, 'S2LESSFMSK')
+            scn_rcd_obj.set_scn_ard(self.params['product_id'], self.params['sensor'], self.params['ard_path'])
+        elif ard_processed:
+            scn_rcd_obj.set_scn_ard(self.params['product_id'], self.params['sensor'], self.params['ard_path'])
+
+        if os.path.exists(self.params['tmp_dir']):
+            shutil.rmtree(self.params['tmp_dir'])
+
+    def required_fields(self, **kwargs):
+        return ["product_id", "sensor", "dwnld_path", "ard_path", "scn_db_file", "tmp_dir", "dem"]
+
+    def outputs_present(self, **kwargs):
+        scn_rcd_obj = RecordScn2Process(self.params['scn_db_file'])
+        ard_processed = scn_rcd_obj.is_scn_ard(self.params['product_id'], self.params['sensor'])
+        return ard_processed, dict()
+
+    def remove_outputs(self, **kwargs):
+        # Reset the tmp dir
+        if os.path.exists(self.params['tmp_dir']):
+            shutil.rmtree(self.params['tmp_dir'])
+        os.mkdir(self.params['tmp_dir'])
+
+        # Remove the output files.
+        scn_rcd_obj = RecordScn2Process(self.params['scn_db_file'])
+        scn_rcd_obj.reset_ard_scn(self.params['product_id'], self.params['sensor'], delpath=True)
+        if not os.path.exists(self.params['ard_path']):
+            os.mkdir(self.params['ard_path'])
+
+
+if __name__ == "__main__":
+    PerformScnARD().std_run()
+
+
